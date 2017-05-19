5000fd6b3ce9287b2fd4342ce98d967040eb77d4
Victor Pryazhnikov <victor@pryazhnikov.com>
2017-05-19 19:43:15 +0300

    Temporary data file copy into data/backup/ folder
    It can be useful in case of network problems on new data loading.

--- input_data_loader.sh
+++ input_data_loader.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+BACKUP_DIR=./data/backup/
 DATA_DIR=./data/temporary
 CURL=`which curl`
 SCRAPY=`which scrapy`
+
+# Current data files saving into backup dir in case of loading errors
+cp -vr "$DATA_DIR" "$BACKUP_DIR"
 
 # 2016
 $CURL 'http://moscowmarathon.org/media/filer_public/16/20160925_mosmarathon_m_42km.json' > $DATA_DIR/2016_male_42km.json
